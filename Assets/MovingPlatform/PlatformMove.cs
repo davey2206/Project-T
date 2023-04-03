fe8bb7ce4f9a1ecc32563dfcd28eae513629188e
davey2206 <davey2206@live.nl>
2023-04-03 13:10:12 +0200

    added a level

--- Assets/MovingPlatform/PlatformMove.cs
+++ Assets/MovingPlatform/PlatformMove.cs
@@ -7,19 +7,29 @@ public class PlatformMove : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] int StartingPoint;
     [SerializeField] Transform[] points;
+    [SerializeField] GameObject platform;
+    [SerializeField] bool moving;
 
     private int i;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = points[StartingPoint].position;
+        platform.transform.position = points[StartingPoint].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, points[i].position) < 0.2f)
+        if (moving)
+        {
+            move();
+        }
+    }
+
+    void move()
+    {
+        if (Vector2.Distance(platform.transform.position, points[i].position) < 0.2f)
         {
             i++;
             if (i == points.Length)
@@ -28,6 +38,11 @@ public class PlatformMove : MonoBehaviour
             }
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        platform.transform.position = Vector2.MoveTowards(platform.transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    public void SwitchMoving()
+    {
+        moving = !moving;
     }
 }
