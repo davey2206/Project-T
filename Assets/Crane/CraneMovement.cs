3bb7dfd65d06e3e42fb7a688f568a64f0fcb8922
Cicilia21 <488119@student.fontys.nl>
2023-04-13 13:06:28 +0200

    crane finish

--- Assets/Crane/CraneMovement.cs
+++ Assets/Crane/CraneMovement.cs
@@ -5,28 +5,51 @@ using UnityEngine;
 public class CraneMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    [SerializeField] ControlPanel panel;
     float vertical, horizontal;
     Rigidbody2D rigidbody2D;
+    GameObject box2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("Player");
-
-        foreach (GameObject go in gameObjectArray)
-        {
-            go.SetActive(false);
-        }
-
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveCrane();
-    }
+        if (panel.GetActiveState())
+        {
+            parent();
+            MoveCrane();
+        }
 
+        else
+        {
+            rigidbody2D.velocity = Vector3.zero;
+        }
+    }
+    private void parent()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && box2D != null)
+        {
+            box2D.transform.parent = GameObject.Find("Persistent").transform;
+            box2D.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            box2D = null;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Box") && box2D == null)
+        {
+            box2D = collision.gameObject;
+            box2D.transform.parent = transform;
+            box2D.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            box2D.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+
+        }
+    }
     void MoveCrane()
     {
         horizontal = Input.GetAxis("Horizontal");
